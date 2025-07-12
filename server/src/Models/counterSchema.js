b413a5355cf56e54ce358c45375e5a643cbd0c58
technoweit77 <technoweit77@gmail.com>
2025-07-12 14:00:42 +0530

    added Counter

---
+++ server/src/Models/counterSchema.js
@@ -0,0 +1,9 @@
+// models/Counter.js
+import mongoose from "mongoose";
+
+const counterSchema = new mongoose.Schema({
+  forCounterName: { type: String, required: true, unique: true },
+  seq: { type: Number, default: 1000 }
+});
+
+export default mongoose.model("Counter", counterSchema);
